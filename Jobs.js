cf32499d6d135268c408f3cf7d7fac57effb21c0
harshgupta271 <124806618+harshgupta271@users.noreply.github.com>
2023-04-22 21:26:46 +0530

    Delete Jobs.js

--- Jobs.js
+++
@@ -1,8 +0,0 @@
-const search=document.querySelector('.search input');
-const filter= (term)=>{
-console.log(term);
-};
-search.addEventListener('keyup',()=>{
-    const term =search.value.trim();
-    filter(term);
-});
